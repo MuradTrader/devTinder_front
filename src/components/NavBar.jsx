c113d7d42c16eb683de80c19ee123a120f2d032f
Мурад Рабаданов <muradrabadanov@MacBook-Air-Murad.local>
2025-01-23 20:16:47 +0300

    front-03

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const user = useSelector((store) => store.user);
@@ -6,36 +7,52 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">🧑‍💻 DevTinder</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          🧑‍💻 DevTinder
+        </Link>
       </div>
       <div className="flex-none gap-2">
-        {user && <p>Добро пожаловать! {user.firstName}</p>}
         {user && (
-          <div className="dropdown dropdown-end mx-2">
+          <div className="badge badge-neutral">
+            Добро пожаловать! {user.firstName}
+          </div>
+        )}
+        {user && (
+          <div className="dropdown dropdown-end mx-2 mt-2">
             <div
               tabIndex={0}
               role="button"
               className="btn btn-ghost btn-circle avatar"
             >
-              <div className="w-10 rounded-full">
-                <img alt="Tailwind CSS Navbar component" src={user.photoUrl} />
-              </div>
+              {user ? (
+                <div className="avatar online">
+                  <div className="rounded-full">
+                    <img src={user.photoUrl} />
+                  </div>
+                </div>
+              ) : (
+                <div className="avatar offline">
+                  <div className=" rounded-full">
+                    <img src={user.photoUrl} />
+                  </div>
+                </div>
+              )}
             </div>
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
-                  Profile
+                <Link to="/profile" className="justify-between">
+                  Профиль
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
-                <a>Settings</a>
+                <a>Настройки</a>
               </li>
               <li>
-                <a>Logout</a>
+                <a className="text-orange-600">Выйти</a>
               </li>
             </ul>
           </div>
